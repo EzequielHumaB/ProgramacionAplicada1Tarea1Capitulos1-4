6c0a8a51829e312e9be03df60c7e7d0cf9cb57f6
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-05-12 11:58:39 -0400

    Eston son los ejercicios del capitulo 1, donde un  problema era escribir nuestro nombre y o tro imprimir mas mensajes en la pantalla

---
+++ ProgramacionAplicada1Tarea1/Capitulo1Ejercicios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgramacionAplicada1Tarea1
+{
+    class Capitulo1Ejercicios
+
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1 del capitulo 1
+            Console.WriteLine("Ezequiel Huma Brito");
+
+            //Ejercicio 5 del capitulo 1
+            Console.WriteLine("Hay un pais en el mundo");
+            Console.WriteLine("Manchester city campeon de la premier");
+            Console.WriteLine("Liverpool elimina al Barca");
+            Console.WriteLine("GSW elimina a Houston");
+            Console.WriteLine("Rusia es el pais mas grande del mundo");
+        }
+    }
+}
